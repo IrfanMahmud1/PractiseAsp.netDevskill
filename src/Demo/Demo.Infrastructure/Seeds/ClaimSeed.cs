23192bf17579935c5b525eb872faa0efbfcb1c8c
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-24 00:02:37 +0600

    add new policy UserAddPermission with a claim add ClaimSeed class add new migration AddClaimSeedData

---
+++ src/Demo/Demo.Infrastructure/Seeds/ClaimSeed.cs
@@ -0,0 +1,25 @@
+﻿using Demo.Infrastructure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Infrastructure.Seeds
+{
+    public static class ClaimSeed
+    {
+        public static ApplicationUserClaim[] GetClaims()
+        {
+            return [
+                new ApplicationUserClaim
+                {
+                    Id = -1,
+                    UserId = new Guid("70F6FA87-0582-41C4-21D2-08DDB20932B7"),
+                    ClaimType = "create_user",
+                    ClaimValue = "allowed"
+                }
+            ];
+        }
+    }
+}
---
+++ src/Demo/Demo.Web/Data/Migrations/20250623084237_AddUserClaimSeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserClaimSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUserClaims",
+                columns: new[] { "Id", "ClaimType", "ClaimValue", "UserId" },
+                values: new object[] { -1, "create_user", "allowed", new Guid("70f6fa87-0582-41c4-21d2-08ddb20932b7") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserClaims",
+                keyColumn: "Id",
+                keyValue: -1);
+        }
+    }
+}
