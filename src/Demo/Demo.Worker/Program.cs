b8aed8e6831cf405524306f2246005042a668d03
IrfanMahmud <irfanmahmud201@gmail.com>
2025-06-25 15:59:31 +0600

    add WorkerModule configure appsettings.json configure autofac, serilog

--- src/Demo/Demo.Worker/Program.cs
+++ src/Demo/Demo.Worker/Program.cs
@@ -1,7 +1,46 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Demo.Worker;
+using Serilog;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", false)
+    .AddEnvironmentVariables()
+    .Build();
 
-var host = builder.Build();
-host.Run();
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+var migrationAssemblyName = typeof(Worker).Assembly.FullName;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Application Starting...");
+    IHost host = Host.CreateDefaultBuilder(args)
+        .UseWindowsService()
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .UseSerilog()
+        .ConfigureContainer<ContainerBuilder>(builder =>
+        {
+            builder.RegisterModule(new WorkerModule(connectionString, migrationAssemblyName));
+        })
+        .ConfigureServices(services =>
+        {
+            services.AddHostedService<Worker>();
+        })
+        .Build();
+
+    await host.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application start up failed");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
